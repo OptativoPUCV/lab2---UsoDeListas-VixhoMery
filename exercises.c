8c138f675596531f5925d127874c3913051ecaa1
Your Name <you@example.com>
2024-03-21 21:10:32 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -99,7 +99,21 @@ El orden de ambas pilas se debe mantener.
 Puedes usar una pila auxiliar.
 */
 
-void copia_pila(Stack* P1, Stack* P2) {
+void copia_pila(Stack* P1, Stack* P2) 
+{
+  Stack* auxiliar = create_stack();
+  void *elemento = top(P1);
+  while(elemento != NULL)
+    {
+      push(auxiliar, elemento);
+      pop(elemento);
+    }
+  void *elem2 = top(auxiliar);
+  while(elem2 != NULL)
+    {
+      push(P2, elem2);
+      pop(elem2);
+    }
 }
 
 /*
